7d6bb897605125fa18a28228db96f54a8c0958b9
Workspace <workspace@Gavishs-MacBook-Pro.local>
2022-06-07 13:30:15 +0100

    init

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './app.css'
+
+import View from './components/layout/View'
+
+import Heading from './components/layout/Heading'
+
+const App = () => {
+  return (
+    <View>
+      <Heading level={1} title={'Pricing Matrix'} />
+      <p>Add your implementation here. See the README for details.</p>
+    </View>
+  )
+}
+
+export default App
